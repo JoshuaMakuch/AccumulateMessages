e1300aa974b77aebdc8f9e155f437028059908e1
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-02-02 09:11:34 -0700

    Added everything including the static variable decleration.

--- AccumulateMessages/MessageProgram.vb
+++ AccumulateMessages/MessageProgram.vb
@@ -1,3 +1,11 @@
+'Joshua Makuch
+'RCET0265
+'Fall 2023
+'Accumulate Messages
+'https://github.com/JoshuaMakuch/AccumulateMessages.git
+
+Option Strict On
+Option Explicit On
 
 Imports System
 
@@ -9,8 +17,20 @@ Module MessageProgram
     End Sub
 
     Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean) As String
-        'your code here
-        Return messages
+
+        Static userMessagesStored As String 'Creates a variable the persists after every function call
+
+        'Determines whether or not the new message is not just a blank line and amends accordingly or clears based on the clear variable
+        If clear Then
+            userMessagesStored = ""
+        ElseIf newMessage = "" Then
+            Return userMessagesStored
+        Else
+            userMessagesStored = userMessagesStored & newMessage & vbCrLf
+        End If
+
+        Return userMessagesStored
+
     End Function
 
 
